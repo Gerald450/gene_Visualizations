414f9cbe193c100908738c2dc8039c992bc11381
Gerald Shimo <shimogerald450@gmail.com>
2025-10-23 15:14:57 -0500

    remove navigation bar

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,13 @@ export default function Home() {
       title: "Common Virulence Genes",
       desc: "Compare gene frequency across jejuni vs coli.",
       tableauSrc:
-        "https://public.tableau.com/views/VirulenceGene_17612389337390/Dash1?:showVizHome=no&:embed=true",
+        "https://public.tableau.com/views/VirulenceGene_17612389337390/Dash1?:showVizHome=no&:embed=true?:showVizHome=no&:embed=true&:tabs=no&:toolbar=no",
     },
     {
       title: "Virulence Roles by Species",
       desc: "Functional roles of virulence genes.",
       tableauSrc:
-        "https://public.tableau.com/views/VirulenceGene_17612389337390/Dash2?:showVizHome=no&:embed=true",
+        "https://public.tableau.com/views/VirulenceGene_17612389337390/Dash2?:showVizHome=no&:embed=true?:showVizHome=no&:embed=true&:tabs=no&:toolbar=no",
     },
     {
       title: "Gene Locations",
