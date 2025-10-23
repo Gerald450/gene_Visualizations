e35c22534ae8a7d04bf3092789b53a6d17705d6f
Gerald Shimo <shimogerald450@gmail.com>
2025-10-23 16:17:43 -0500

    create data file

---
+++ app/data/visuals.ts
@@ -0,0 +1,33 @@
+export const visuals = [
+    {
+      title: "Common Virulence Genes",
+      desc: "Compare gene frequency across jejuni vs coli.",
+      tableauSrc:
+        "https://public.tableau.com/views/VirulenceGene_17612389337390/Dash1?:showVizHome=no&:embed=true?:showVizHome=no&:embed=true&:tabs=no&:toolbar=no",
+    },
+    {
+      title: "Virulence Roles by Species",
+      desc: "Functional roles of virulence genes.",
+      tableauSrc:
+        "https://public.tableau.com/views/VirulenceGene_17612389337390/Dash2?:showVizHome=no&:embed=true?:showVizHome=no&:embed=true&:tabs=no&:toolbar=no",
+    },
+    {
+      title: "Gene Locations",
+      desc: "Where virulence genes lie along the chromosome.",
+      tableauSrc:
+        "https://public.tableau.com/views/VirulenceGene_17612389337390/Dash3?:showVizHome=no&:embed=true?:showVizHome=no&:embed=true&:tabs=no&:toolbar=no",
+    },
+    {
+      title: "Host–Function Relationship",
+      desc: "Link hosts to virulence traits.",
+      tableauSrc:
+        "https://public.tableau.com/views/VirulenceGene_17612389337390/Dash4?:showVizHome=no&:embed=true?:showVizHome=no&:embed=true&:tabs=no&:toolbar=no",
+    },
+    {
+      title: "Universal Virulence Genes",
+      desc: "Genes present across all hosts.",
+      tableauSrc:
+        "https://public.tableau.com/views/VirulenceGene_17612389337390/Dash5?:showVizHome=no&:embed=true?:showVizHome=no&:embed=true&:tabs=no&:toolbar=no",
+    },
+
+]
